47ddb32e4d7984d1e3f468dc2758a31d1d2f8147
Den Hoopingarner <denniehoopingarner@icloud.com>
2023-02-22 17:11:14 +0800

    22Feb001

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <h1>Hi from the App</h1>;
+  const title = 'Dennie is King!';
+  const body = "And here's why:";
+  const reasons = [
+    { id: 1, reason: 'smart' },
+    { id: 3, reason: 'funny' },
+    { id: 2, reason: 'rich' },
+    { id: 4, reason: 'tall' },
+  ];
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>
+        {body} ({reasons.length})
+      </p>
+      <ol>
+        {reasons.map((reason, idx) => (
+          <li key={idx}>{reason.reason}</li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
 export default App;
